be52a1bc2a9fef38d4cced2d7e050a1492441477
noamteyssier <22600644+noamteyssier@users.noreply.github.com>
2022-08-24 12:16:32 -0700

    added cli parser, io, and fdr correction

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,55 @@
 use alpha_rra::alpha_rra;
-use ndarray::array;
+use clap::Parser;
+use anyhow::Result;
+use adjustp::{adjust, Procedure};
 
-fn main() {
-    let pvalues = array![
-        0.3, 0.2, 1e-3, 1e-2, 1e-3, 0.4, 1e-2, 0.4
-    ];
-    let genes: Vec<String> = vec![
-        "gene.0", "gene.0", "gene.1", "gene.1", "gene.1", "gene.2", "gene.2", "gene.2" 
-    ].iter().map(|x| x.to_string()).collect();
+mod io;
+use io::Input;
 
-    let (unique_genes, scores, pvalues) = alpha_rra(&pvalues, &genes, 0.3, 10000);
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+
+    /// Filepath of the input count matrix
+    #[clap(short, long, value_parser)]
+    input: String,
+
+    /// Permutations
+    #[clap(short, long, value_parser, default_value="100")]
+    permutations: usize,
+
+    /// Alpha Threshold
+    #[clap(short, long, value_parser, default_value="0.1")]
+    alpha: f64,
+
+    /// Multiple Hypothesis Correction (bonferroni, bh, by)
+    #[clap(short='f', long, value_parser, default_value="bh")]
+    correction: String
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let input = Input::from_path(&args.input)?;
+    let correction = match args.correction.as_str() {
+        "bonferroni" => Procedure::Bonferroni,
+        "bh" | "fdr" => Procedure::BenjaminiHochberg,
+        "by" => Procedure::BenjaminiYekutieli,
+        _ => panic!("Unexpected correction method provided: {}", args.correction)
+    };
+
+    let (unique_genes, scores, pvalues) = alpha_rra(
+        input.pvalues(), 
+        input.genes(), 
+        args.alpha, 
+        args.permutations
+    );
+
+    let fdr = adjust(&pvalues.as_slice().unwrap(), correction);
+
     println!("{:?}", unique_genes);
     println!("{:?}", scores);
     println!("{:?}", pvalues);
+    println!("{:?}", fdr);
+
+    Ok(())
 }
