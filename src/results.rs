bcb965eccf73d305ccccf57fdb06e493b44054fb
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-06 14:35:09 -0700

    added docstrings to results

--- src/results.rs
+++ src/results.rs
@@ -1,12 +1,16 @@
 use adjustp::{Procedure, adjust};
 
+/// Handles the results of an Alpha-RRA run
 pub struct ResultsRRA {
     names: Vec<String>,
     scores: Vec<f64>,
     pvalues: Vec<f64>,
     adj_pvalues: Vec<f64>
 }
+
 impl ResultsRRA {
+
+    /// Creates a new instance
     pub fn new(names: Vec<String>, scores: Vec<f64>, pvalues: Vec<f64>, correction: Procedure) -> Self {
         Self {
             adj_pvalues: adjust(&pvalues, correction),
@@ -14,18 +18,22 @@ impl ResultsRRA {
         }
     }
 
+    /// Gets the internal names
     pub fn names(&self) -> &Vec<String> {
         &self.names
     }
 
+    /// Gets the internal scores
     pub fn scores(&self) -> &Vec<f64> {
         &self.scores
     }
 
+    /// Gets the internal pvalues
     pub fn pvalues(&self) -> &Vec<f64> {
         &self.pvalues
     }
 
+    /// Gets the internal adjusted pvalues
     pub fn adj_pvalues(&self) -> &Vec<f64> {
         &self.adj_pvalues
     }
