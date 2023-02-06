8f934af1715d211d2b9943eac1b7068733da3577
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-02-06 09:51:42 -0800

    renamed alpha_rra to arra to avoid name collisions

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
-pub mod alpha_rra;
+pub mod arra;
 pub mod permutations;
 pub mod results;
 pub mod robust_rank;
 pub mod utils;
 
-pub use alpha_rra::alpha_rra;
+pub use arra::alpha_rra;
 pub use results::ResultsRRA;
 use robust_rank::robust_rank_aggregation;
 use utils::{filter_alpha, group_sizes, normed_ranks};
