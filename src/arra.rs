1ec358797b89febc7eacdf632060e202bd21cfc2
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-02-06 10:26:33 -0800

    moved logical steps into separate functions

--- src/arra.rs
+++ src/arra.rs
@@ -43,6 +43,57 @@ fn gene_rra(
     )
 }
 
+/// Generates a vector of random samplings for each unique size then scores them
+///
+/// # Arguments
+/// * `n_genes` - The number of genes
+/// * `alpha` - The alpha threshold value
+/// * `npermutations` - The number of permutations
+/// * `sizes` - The unique sizes of each group
+///
+/// # Returns
+/// A map of the unique sizes to the random samplings scores
+fn generate_permutation_vectors(
+    n_genes: usize,
+    alpha: f64,
+    npermutations: usize,
+    sizes: &Vec<usize>,
+) -> HashMap<usize, Array1<f64>> {
+    sizes
+        .iter()
+        .map(|unique_size| {
+            (
+                *unique_size,
+                run_permutations(n_genes, alpha, npermutations, *unique_size),
+            )
+        })
+        .map(|(u, v)| (u, Array1::from_vec(v)))
+        .collect::<HashMap<usize, Array1<f64>>>()
+}
+
+/// Calculates the empirical p-values for each of the gene sets given the random nulls
+///
+/// # Arguments
+/// * `n_genes` - The number of genes
+/// * `encode` - The encodings (i.e. gene indices)
+/// * `nranks` - The normalized ranks
+/// * `permutation_vectors` - The permutation vectors
+/// * `alpha` - The alpha threshold value
+///
+/// # Returns
+/// A tuple of the scores and the empirical p-values
+fn calculate_empirical_pvalues(
+    n_genes: usize,
+    encode: &[usize],
+    nranks: &Array1<f64>,
+    permutation_vectors: &HashMap<usize, Array1<f64>>,
+    alpha: f64,
+) -> (Vec<f64>, Vec<f64>) {
+    (0..n_genes)
+        .map(|curr| gene_rra(curr, &encode, &nranks, &permutation_vectors, alpha))
+        .unzip()
+}
+
 /// Performs the alpha-RRA algorithm
 #[must_use]
 pub fn alpha_rra(
@@ -63,21 +114,10 @@ pub fn alpha_rra(
     let sizes = group_sizes(&encode);
 
     // calculate rra scores for a vector of random samplings for each unique size
-    let permutation_vectors = sizes
-        .iter()
-        .map(|unique_size| {
-            (
-                *unique_size,
-                run_permutations(nranks.len(), alpha, npermutations * n_genes, *unique_size),
-            )
-        })
-        .map(|(u, v)| (u, Array1::from_vec(v)))
-        .collect::<HashMap<usize, Array1<f64>>>();
+    let permutation_vectors = generate_permutation_vectors(n_genes, alpha, npermutations, &sizes);
 
     // calculate empirical pvalues for each of the gene sets given the random nulls
-    let (scores, pvalues): (Vec<f64>, Vec<f64>) = (0..n_genes)
-        .map(|curr| gene_rra(curr, &encode, &nranks, &permutation_vectors, alpha))
-        .unzip();
+    let (scores, pvalues) = calculate_empirical_pvalues(n_genes, &encode, &nranks, &permutation_vectors, alpha);
 
     // recode the gene names
     let names = recode_index(n_genes, &encode_map);
