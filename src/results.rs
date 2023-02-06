35c28693cc1a3317cd5b23a2bf1f5eb710cd662d
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-02-06 10:07:11 -0800

    14 improve testing coverage (#15)
    * added tessting on rra results
    
    * ran cargo formatting

--- src/results.rs
+++ src/results.rs
@@ -1,6 +1,7 @@
 use adjustp::{adjust, Procedure};
 
-type EntryRRA<'a> = (&'a String, f64, f64, f64);
+/// Type alias for the results of an Alpha-RRA run for a single entry
+type EntryRRA<'a> = (&'a str, f64, f64, f64);
 
 /// Handles the results of an Alpha-RRA run
 pub struct ResultsRRA {
@@ -46,14 +47,40 @@ impl ResultsRRA {
         &self.adj_pvalues
     }
 
+    /// zips the results into a single iterator
     pub fn zip<'a>(&'a self) -> impl Iterator<Item = EntryRRA<'a>> {
         (0..self.names.len()).map(|ix| {
             (
-                &self.names[ix],
+                self.names[ix].as_str(),
                 self.scores[ix],
                 self.pvalues[ix],
                 self.adj_pvalues[ix],
             )
         })
+    }
+}
+
+#[cfg(test)]
+mod testing {
+
+    #[test]
+    fn test_results() {
+        use super::*;
+        use adjustp::Procedure;
+
+        let names = vec!["a".to_string(), "b".to_string()];
+        let scores = vec![1.0, 2.0];
+        let pvalues = vec![0.1, 0.2];
+        let results = ResultsRRA::new(names, scores, pvalues, Procedure::BenjaminiHochberg);
+
+        assert_eq!(results.names().len(), 2);
+        assert_eq!(results.scores().len(), 2);
+        assert_eq!(results.pvalues().len(), 2);
+        assert_eq!(results.adj_pvalues().len(), 2);
+
+        let mut iter = results.zip();
+        assert_eq!(iter.next(), Some(("a", 1.0, 0.1, 0.2)));
+        assert_eq!(iter.next(), Some(("b", 2.0, 0.2, 0.2)));
+        assert_eq!(iter.next(), None);
     }
 }
