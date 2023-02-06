5f513468e99d085e17af80db494328cb68ec2557
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-02-06 09:46:48 -0800

    renamed arra.rs to alpha_rra.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
-pub mod arra;
+pub mod alpha_rra;
 pub mod robust_rank;
 pub mod utils;
 pub mod permutations;
 pub mod results;
 
-pub use arra::alpha_rra;
+pub use alpha_rra::alpha_rra;
 pub use results::ResultsRRA;
 use robust_rank::robust_rank_aggregation;
 use utils::{normed_ranks, group_sizes, filter_alpha};
